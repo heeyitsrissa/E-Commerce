5f6300b9e029de1e119b90bca7c07891eabc0b5d
Marissa Pratt <131078835+heeyitsrissa@users.noreply.github.com>
2024-03-17 15:39:40 -0400

    added all model codes

--- models/index.js
+++ models/index.js
@@ -5,13 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foriegnKey: 'category_id',
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foriegnKey: 'category_id',
+  onDelete: 'CASCADE',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag, {
+  foriegnKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(ProductTag, {
+  foriegnKey:'product_id',
+  
+})
 module.exports = {
   Product,
   Category,
